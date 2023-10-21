64a8d4a5ccb7a5c41edd5bfa2615b042024ee6fe
Ale478 <alemusicmolin@gmail.com>
2023-10-21 12:18:24 -0300

    Agrega clase UseEffect

---
+++ clase-UseEffect/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+import Simple from "./componentes/Simple";
+import CicloVida from "./componentes/CicloVida";
+import CleanUp from "./componentes/CleanUp";
+
+function App() {
+  return (
+    <div className="App">
+        {/*<Simple />*/}
+        {/*<CicloVida/>*/}
+        <CleanUp/>
+    </div>
+  );
+}
+
+export default App;
